5785c759041e7f8ca632080c1619a76d398d3de8
You have one new message <gabriel_pirastru@hotmail.fr>
2025-12-11 22:05:06 +0700

    routing

---
+++ spoke-staging/05-peering.tf
@@ -0,0 +1,70 @@
+# spoke-staging/05-peering.tf
+# VNet Peering between Staging Spoke and Hub
+
+# ============================================================================
+# Naming Modules
+# ============================================================================
+
+module "spoke_to_hub_peering_naming" {
+  source = "../modules/naming"
+
+  resource_type = "peer"
+  workload      = "staging-to-hub"
+  environment   = var.environment
+  location      = var.location
+  instance      = "001"
+  common_tags   = var.tags
+}
+
+module "hub_to_spoke_peering_naming" {
+  source = "../modules/naming"
+
+  resource_type = "peer"
+  workload      = "hub-to-staging"
+  environment   = var.environment
+  location      = var.location
+  instance      = "001"
+  common_tags   = var.tags
+}
+
+# ============================================================================
+# VNet Peering: Spoke to Hub
+# ============================================================================
+
+resource "azurerm_virtual_network_peering" "spoke_to_hub" {
+  name                      = module.spoke_to_hub_peering_naming.name
+  resource_group_name       = module.rg_spoke.rg_name
+  virtual_network_name      = module.spoke_vnet.vnet_name
+  remote_virtual_network_id = local.hub_vnet_id
+
+  # Spoke Configuration
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true  # Allow traffic forwarded by hub firewall
+  allow_gateway_transit        = false # Spoke doesn't provide gateway
+  use_remote_gateways          = false # Set to true if hub has VPN/ExpressRoute gateway
+
+  depends_on = [
+    module.spoke_vnet
+  ]
+}
+
+# ============================================================================
+# VNet Peering: Hub to Spoke
+# ============================================================================
+
+resource "azurerm_virtual_network_peering" "hub_to_spoke" {
+  name                      = module.hub_to_spoke_peering_naming.name
+  resource_group_name       = var.hub_resource_group_name
+  virtual_network_name      = local.hub_vnet_name
+  remote_virtual_network_id = module.spoke_vnet.vnet_id
+
+  # Hub Configuration
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true  # Allow hub to forward traffic to spoke
+  allow_gateway_transit        = false # Set to true if hub has VPN/ExpressRoute gateway
+  use_remote_gateways          = false # Hub provides gateway, doesn't use remote
+
+  depends_on = [
+    module.spoke_vnet
+  ]
+}
---
+++ spoke-staging/99-outputs.tf
@@ -0,0 +1,124 @@
+# spoke-staging/99-outputs.tf
+# Outputs from Staging Spoke configuration
+
+# ============================================================================
+# Resource Group Outputs
+# ============================================================================
+
+output "resource_group_id" {
+  value       = module.rg_spoke.rg_id
+  description = "Staging spoke resource group ID"
+}
+
+output "resource_group_name" {
+  value       = module.rg_spoke.rg_name
+  description = "Staging spoke resource group name"
+}
+
+# ============================================================================
+# VNet Outputs
+# ============================================================================
+
+output "vnet_id" {
+  value       = module.spoke_vnet.vnet_id
+  description = "Staging spoke VNet ID"
+}
+
+output "vnet_name" {
+  value       = module.spoke_vnet.vnet_name
+  description = "Staging spoke VNet name"
+}
+
+output "vnet_address_space" {
+  value       = module.spoke_vnet.vnet_address_space
+  description = "Staging spoke VNet address space"
+}
+
+# ============================================================================
+# Subnet Outputs
+# ============================================================================
+
+output "workload_subnet_id" {
+  value       = local.deploy_workload_subnet ? module.workload_subnet[0].subnet_id : null
+  description = "Workload subnet ID (if deployed)"
+}
+
+output "data_subnet_id" {
+  value       = local.deploy_data_subnet ? module.data_subnet[0].subnet_id : null
+  description = "Data subnet ID (if deployed)"
+}
+
+output "app_subnet_id" {
+  value       = local.deploy_app_subnet ? module.app_subnet[0].subnet_id : null
+  description = "Application subnet ID (if deployed)"
+}
+
+# ============================================================================
+# NSG Outputs
+# ============================================================================
+
+output "workload_nsg_id" {
+  value       = local.deploy_workload_subnet ? module.workload_nsg[0].nsg_id : null
+  description = "Workload NSG ID (if deployed)"
+}
+
+output "data_nsg_id" {
+  value       = local.deploy_data_subnet ? module.data_nsg[0].nsg_id : null
+  description = "Data NSG ID (if deployed)"
+}
+
+output "app_nsg_id" {
+  value       = local.deploy_app_subnet ? module.app_nsg[0].nsg_id : null
+  description = "Application NSG ID (if deployed)"
+}
+
+# ============================================================================
+# Route Table Outputs
+# ============================================================================
+
+output "workload_route_table_id" {
+  value       = local.deploy_workload_subnet && local.enable_forced_tunneling ? module.workload_route_table[0].route_table_id : null
+  description = "Workload route table ID (if deployed)"
+}
+
+output "data_route_table_id" {
+  value       = local.deploy_data_subnet && local.enable_forced_tunneling ? module.data_route_table[0].route_table_id : null
+  description = "Data route table ID (if deployed)"
+}
+
+output "app_route_table_id" {
+  value       = local.deploy_app_subnet && local.enable_forced_tunneling ? module.app_route_table[0].route_table_id : null
+  description = "Application route table ID (if deployed)"
+}
+
+# ============================================================================
+# Peering Outputs
+# ============================================================================
+
+output "spoke_to_hub_peering_id" {
+  value       = azurerm_virtual_network_peering.spoke_to_hub.id
+  description = "Spoke-to-Hub peering ID"
+}
+
+output "hub_to_spoke_peering_id" {
+  value       = azurerm_virtual_network_peering.hub_to_spoke.id
+  description = "Hub-to-Spoke peering ID"
+}
+
+# ============================================================================
+# Summary Output
+# ============================================================================
+
+output "spoke_summary" {
+  value = {
+    resource_group      = module.rg_spoke.rg_name
+    vnet_name           = module.spoke_vnet.vnet_name
+    vnet_address_space  = local.spoke_address_space
+    workload_deployed   = local.deploy_workload_subnet
+    data_deployed       = local.deploy_data_subnet
+    app_deployed        = local.deploy_app_subnet
+    forced_tunneling    = local.enable_forced_tunneling
+    peering_to_hub      = azurerm_virtual_network_peering.spoke_to_hub.name
+  }
+  description = "Staging spoke configuration summary"
+}
