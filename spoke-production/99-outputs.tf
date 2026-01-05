f71476c44ea91d5e689924bd4a5d23b813ca53b9
You have one new message <gabriel_pirastru@hotmail.fr>
2026-01-05 21:22:29 +0700

    outputs

--- spoke-production/99-outputs.tf
+++ spoke-production/99-outputs.tf
@@ -35,60 +35,41 @@ output "vnet_address_space" {
 }
 
 # ============================================================================
-# Subnet Outputs
+# ACI Subnet Outputs
 # ============================================================================
 
-output "workload_subnet_id" {
-  value       = local.deploy_workload_subnet ? module.workload_subnet[0].subnet_id : null
-  description = "Workload subnet ID (if deployed)"
+output "aci_subnet_id" {
+  value       = local.deploy_aci_subnet ? module.aci_subnet[0].subnet_id : null
+  description = "ACI subnet ID (if deployed)"
 }
 
-output "data_subnet_id" {
-  value       = local.deploy_data_subnet ? module.data_subnet[0].subnet_id : null
-  description = "Data subnet ID (if deployed)"
+output "aci_nsg_id" {
+  value       = local.deploy_aci_subnet ? module.aci_nsg[0].nsg_id : null
+  description = "ACI NSG ID (if deployed)"
 }
 
-output "app_subnet_id" {
-  value       = local.deploy_app_subnet ? module.app_subnet[0].subnet_id : null
-  description = "Application subnet ID (if deployed)"
-}
-
-# ============================================================================
-# NSG Outputs
-# ============================================================================
-
-output "workload_nsg_id" {
-  value       = local.deploy_workload_subnet ? module.workload_nsg[0].nsg_id : null
-  description = "Workload NSG ID (if deployed)"
-}
-
-output "data_nsg_id" {
-  value       = local.deploy_data_subnet ? module.data_nsg[0].nsg_id : null
-  description = "Data NSG ID (if deployed)"
-}
-
-output "app_nsg_id" {
-  value       = local.deploy_app_subnet ? module.app_nsg[0].nsg_id : null
-  description = "Application NSG ID (if deployed)"
+output "aci_route_table_id" {
+  value       = local.deploy_aci_subnet && local.enable_forced_tunneling ? module.aci_route_table[0].route_table_id : null
+  description = "ACI route table ID (if deployed)"
 }
 
 # ============================================================================
-# Route Table Outputs
+# ACI Container Instance Outputs
 # ============================================================================
 
-output "workload_route_table_id" {
-  value       = local.deploy_workload_subnet && local.enable_forced_tunneling ? module.workload_route_table[0].route_table_id : null
-  description = "Workload route table ID (if deployed)"
+output "aci_id" {
+  value       = local.deploy_aci_subnet ? module.aci[0].aci_id : null
+  description = "ACI container group ID (if deployed)"
 }
 
-output "data_route_table_id" {
-  value       = local.deploy_data_subnet && local.enable_forced_tunneling ? module.data_route_table[0].route_table_id : null
-  description = "Data route table ID (if deployed)"
+output "aci_ip_address" {
+  value       = local.deploy_aci_subnet ? module.aci[0].aci_ip_address : null
+  description = "ACI container group IP address (if deployed)"
 }
 
-output "app_route_table_id" {
-  value       = local.deploy_app_subnet && local.enable_forced_tunneling ? module.app_route_table[0].route_table_id : null
-  description = "Application route table ID (if deployed)"
+output "aci_fqdn" {
+  value       = local.deploy_aci_subnet ? module.aci[0].aci_fqdn : null
+  description = "ACI container group FQDN (if deployed)"
 }
 
 # ============================================================================
@@ -106,34 +87,18 @@ output "hub_to_spoke_peering_id" {
 }
 
 # ============================================================================
-# Test VM Outputs
-# ============================================================================
-
-output "test_vm_id" {
-  value       = local.deploy_workload_subnet && length(module.test_vm) > 0 ? module.test_vm[0].vm_id : null
-  description = "Test VM resource ID"
-}
-
-output "test_vm_private_ip" {
-  value       = local.deploy_workload_subnet && length(module.test_vm) > 0 ? module.test_vm[0].vm_private_ip : null
-  description = "Test VM private IP address"
-}
-
-# ============================================================================
 # Summary Output
 # ============================================================================
 
 output "spoke_summary" {
   value = {
-    resource_group      = module.rg_spoke.rg_name
-    vnet_name           = module.spoke_vnet.vnet_name
-    vnet_address_space  = local.spoke_address_space
-    workload_deployed   = local.deploy_workload_subnet
-    data_deployed       = local.deploy_data_subnet
-    app_deployed        = local.deploy_app_subnet
-    forced_tunneling    = local.enable_forced_tunneling
-    resource_lock       = var.enable_resource_lock
-    peering_to_hub      = azurerm_virtual_network_peering.spoke_to_hub.name
+    resource_group     = module.rg_spoke.rg_name
+    vnet_name          = module.spoke_vnet.vnet_name
+    vnet_address_space = local.spoke_address_space
+    aci_deployed       = local.deploy_aci_subnet
+    forced_tunneling   = local.enable_forced_tunneling
+    resource_lock      = var.enable_resource_lock
+    peering_to_hub     = azurerm_virtual_network_peering.spoke_to_hub.name
   }
   description = "Production spoke configuration summary"
 }
