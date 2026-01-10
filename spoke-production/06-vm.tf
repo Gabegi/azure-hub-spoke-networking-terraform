35c94bd6fde3e19621872328e26644b6a43657f8
You have one new message <gabriel_pirastru@hotmail.fr>
2026-01-10 14:24:48 +0700

    vms

--- spoke-production/06-vm.tf
+++ spoke-production/06-vm.tf
@@ -21,10 +21,16 @@ module "vm" {
   subnet_id           = module.vm_subnet.subnet_id
 
   # VM Configuration
-  vm_size              = "Standard_B2s"
+  vm_size              = "Standard_D2s_v3"
   admin_username       = "azureuser"
   admin_ssh_public_key = var.vm_admin_ssh_public_key
-  availability_zones   = []  # B-series VMs don't require availability zones
+  availability_zones   = ["1"]  # Try zone 1 for better availability
+
+  # OS Disk Configuration
+  os_disk_storage_account_type = "Premium_LRS"  # D-series supports Premium storage
+
+  # OS Image - Gen2 for better performance
+  source_image_sku = "22_04-lts-gen2"
 
   # Network Configuration
   enable_public_ip = false  # Private IP only - no public IP
